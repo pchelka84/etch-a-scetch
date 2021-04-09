ee9ceaf99963a41b8d27ef55bf4be4ba274e0757
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-04-09 13:56:06 -0400

    Select elements, define functions

---
+++ script.js
@@ -0,0 +1,24 @@
+const scetchGrid = document.querySelector('.scetch-container');
+const changeSizeBtn = document.getElementById('change-size');
+const clearBtn = document.getElementById('clear');
+
+// Set grid 
+function setGrid(size) {
+
+}
+
+// Change grid size
+function changeGridSize(size) {
+
+}
+
+// Clear grid 
+function clearGrid() {
+
+}
+
+// Event listeners
+changeSizeBtn.addEventListener('click', changeGridSize);
+clearBtn.addEventListener('click', clearGrid);
+
+setGrid(14);
